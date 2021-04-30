f5dd9f9946f8e8549d9645e232a65ec7f4de84e6
Brandon Bench <brandon.bench@tcnbroadcasting.com>
2021-04-30 15:14:28 -0600

    don't use 0 as default value

--- main.go
+++ main.go
@@ -6,38 +6,49 @@ import (
 	"strings"
 )
 
+// Bytes with value 0 are not shown in string but do change the hash.
+// 0 should not be used as value.
+
 func main() {
 	bytes := make([]byte, 3) // 64 is max length
+  for i := range bytes {
+    bytes[i] = 32 // initialize all values as 32 (space character)
+  }
+
 	increment(0, bytes)
 }
 
 func increment(index int, bytes []byte) {
 	if index == len(bytes)-1 {
-		// printable ascii starts at 32. treat 31 as newline (ascii 10)
-		for i := 31; i < 128; i++ {
+    // TODO: break this out into seperate func instead of
+    // copy/pasting same code below.
+
+		// printable ascii starts at 32 and ends at 127.
+    // treat 128 as newline (ascii 10)
+		for i := 32; i < 129; i++ {
 			bytes[index] = byte(i)
-			if i == 31 {
+			if i == 128 {
 				bytes[index] = byte(10)
 			}
 			hash(bytes)
 		}
 
-		// reset byte back to 0
-		bytes[index] = byte(0)
+		// reset byte back to 32
+		bytes[index] = byte(32)
 		return
 	}
 
-	for i := 31; i < 128; i++ {
+	for i := 32; i < 129; i++ {
 		increment(index+1, bytes)
 		bytes[index] = byte(i)
-		if i == 31 {
+		if i == 128 {
 			bytes[index] = byte(10)
 		}
 		hash(bytes)
 	}
 
-	// reset byte back to 0
-	bytes[index] = byte(0)
+	// reset byte back to 32
+	bytes[index] = byte(32)
 	return
 }
 
