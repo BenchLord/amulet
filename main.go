7ec99eff1d40998fb5da9ce8def75711beb70817
Brandon Bench <brandon.bench@tcnbroadcasting.com>
2021-04-30 15:59:04 -0600

    add flag and split out into new function

--- main.go
+++ main.go
@@ -3,52 +3,60 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+  "flag"
 	"strings"
+  "os"
 )
 
 // Bytes with value 0 are not shown in string but do change the hash.
 // 0 should not be used as value.
 
+var start = flag.String("start", "", "The starting bytes for the miner to increment. (Default is 32 spaces)")
+
 func main() {
-	bytes := make([]byte, 3) // 64 is max length
-  for i := range bytes {
-    bytes[i] = 32 // initialize all values as 32 (space character)
+  flag.Parse()
+  bytes, err := getStartingBytes(*start)
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
   }
 
 	increment(0, bytes)
 }
 
+func getStartingBytes(s string) ([]byte, error) {
+  var out []byte
+  if s == "" {
+    out = make([]byte, 32)
+    for i := range out {
+      out[i] = 32
+    }
+
+    return out, nil
+  }
+
+  out = []byte(s)
+  if len(out) > 64 {
+    return nil, fmt.Errorf("start cannot be greater than 64 bytes")
+  }
+
+  return out, nil
+}
+
 func increment(index int, bytes []byte) {
+  f := func() {
+    increment(index+1, bytes)
+    hash(bytes)
+  }
+
 	if index == len(bytes)-1 {
-    // TODO: break this out into seperate func instead of
-    // copy/pasting same code below.
-
-		// printable ascii starts at 32 and ends at 127.
-    // treat 128 as newline (ascii 10)
-		for i := 32; i < 129; i++ {
-			bytes[index] = byte(i)
-			if i == 128 {
-				bytes[index] = byte(10)
-			}
-			hash(bytes)
-		}
-
-		// reset byte back to 32
-		bytes[index] = byte(32)
-		return
+    f = func() {
+      hash(bytes)
+    }
 	}
 
-	for i := 32; i < 129; i++ {
-		increment(index+1, bytes)
-		bytes[index] = byte(i)
-		if i == 128 {
-			bytes[index] = byte(10)
-		}
-		hash(bytes)
-	}
 
-	// reset byte back to 32
-	bytes[index] = byte(32)
+  incrementByte(&bytes[index], f)
 	return
 }
 
@@ -61,3 +69,17 @@ func hash(bytes []byte) {
 	}
 }
 
+func incrementByte(b *byte, f func()) {
+  // printable ascii starts at 32 and ends at 127.
+  // treat 128 as newline (ascii 10)
+  for i := 32; i < 129; i++ {
+    *b = byte(i)
+    if *b == 128 {
+      *b = 10
+    }
+    f()
+  }
+
+  // reset byte back to 32
+  *b = byte(32)
+}
